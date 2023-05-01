75025e154c318ab534226abc7915634531b04758
dark horse <asharma13524@gmail.com>
2023-05-01 11:05:25 -0400

    Add config for cloud prover

--- backend/src/aleo/sudoku.ts
+++ backend/src/aleo/sudoku.ts
@@ -9,25 +9,33 @@ router.post('/', async (req: Request, res: Response) => {
     console.log(`${req.originalUrl} called with ${JSON.stringify(req.body)}`);
     if (req.body.private_key === "" || req.body.private_key == null) {
         res.json({ error: "Player private_key is required" });
-        return
+        return;
     }
 
     if (req.body.puzzle === "" || req.body.puzzle == null) {
         res.json({ error: "Puzzle is required" });
-        return
-    }
-    
-    if (req.body.solution === "" || req.body.solution == null) {
-        res.json({ error: "Solution is required" });
-        return
+        return;
     }
 
-    const cmd = `snarkos developer execute sudoku.aleo solve_puzzle "${convertSudokuBoardToAleoPuzzleInfo(req.body.puzzle)}" "${convertSudokuBoardToAleoPuzzleInfo(req.body.solution)}" --private-key ${req.body.private_key} --query "http://localhost:3030" --broadcast "http://localhost:3030/testnet3/transaction/broadcast"`;
+    if (req.body.solution === "" || req.body.solution == null) {
+        res.json({ error: "Solution is required" });
+        return;
+    }
+    const awsInstance = process.env.AWS_INSTANCE; // IP address of EC2 Prover
+    // add pem file to backend/
+    const cmd = `ssh -i "aleo.pem" ${awsInstance} "sudo su && cd /home/ubuntu/Zero-Knowledge-Sudoku-Wordle-Trivia/aleo/sudoku && snarkos developer execute sudoku.aleo solve_puzzle '${convertSudokuBoardToAleoPuzzleInfo(
+        req.body.puzzle
+    )}' '${convertSudokuBoardToAleoPuzzleInfo(
+        req.body.solution
+    )}' --private-key '${
+        req.body.private_key
+    }' --query 'http://localhost:3030' --broadcast 'http://localhost:3030/testnet3/transaction/broadcast'"`;
+
     const { stdout, stderr } = await exec(cmd);
     console.log(`stdout: ${stdout}`);
     console.log(`stderr: ${stderr}`);
 
-    res.json({ output: stdout });    
+    res.json({ output: stdout });
 });
 
 export default router;
